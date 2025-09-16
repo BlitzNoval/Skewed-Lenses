7c2862dd2a1b7af8d92af4fe9604e08328bc788b
BlitzNoval <ljmoodley03@gmail.com>
2025-09-17 00:33:18 +0200

    Vite & React Set Up
    Vite Integration , Claude Integration , React Integration , Folder Set Up , Whisper Model Imported

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Skewed Lenses</h1>
+      <p>Dyslexia Screening Tool</p>
+    </div>
+  )
+}
+
+export default App
